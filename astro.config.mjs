4b582535674afe845f2fb0694ca85f9d1c8b794a
niuqiang1 <1228918562@qq.com>
2023-03-13 10:05:08 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://niuqiang1.github.io',
+	base:'/hhz2',
 	integrations: [mdx(), sitemap()],
 });
